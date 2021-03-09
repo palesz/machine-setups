5056d89f1e70420d31b6091d064abe20ce60e1e8
Andras Palinkas <andras.palinkas@elastic.co>
2021-03-09 17:36:15 -0500

    Delete should not modify the kill-ring in emacs

--- emacs/init.el
+++ emacs/init.el
@@ -128,6 +128,54 @@
 (transient-mark-mode 1)
 (delete-selection-mode 1)
 
+;; don't copy after mouse selection
+(setq mouse-drag-copy-region nil)
+(setq select-active-regions nil)
+
+;; Don't add to the kill-ring on delete
+(defun my-delete-word (arg)
+  "Delete characters forward until encountering the end of a word.
+With argument, do this that many times.
+This command does not push text to `kill-ring'."
+  (interactive "p")
+  (delete-region
+   (point)
+   (progn
+     (forward-word arg)
+     (point))))
+
+(defun my-backward-delete-word (arg)
+  "Delete characters backward until encountering the beginning of a word.
+With argument, do this that many times.
+This command does not push text to `kill-ring'."
+  (interactive "p")
+  (my-delete-word (- arg)))
+
+(defun my-delete-line ()
+  "Delete text from current position to end of line char.
+This command does not push text to `kill-ring'."
+  (interactive)
+  (delete-region
+   (point)
+   (progn (end-of-line 1) (point)))
+  (delete-char 1))
+
+(defun my-delete-line-backward ()
+  "Delete text between the beginning of the line to the cursor position.
+This command does not push text to `kill-ring'."
+  (interactive)
+  (let (p1 p2)
+    (setq p1 (point))
+    (beginning-of-line 1)
+    (setq p2 (point))
+    (delete-region p1 p2)))
+
+; bind them to emacs's default shortcut keys:
+(global-set-key (kbd "C-S-k") 'my-delete-line-backward) ; Ctrl+Shift+k
+(global-set-key (kbd "C-k") 'my-delete-line)
+(global-set-key (kbd "M-d") 'my-delete-word)
+(global-set-key (kbd "<M-backspace>") 'my-backward-delete-word)
+
 ;; Highlight the current line
 (global-hl-line-mode 1)
 (set-face-background hl-line-face "#eeeeee" )
